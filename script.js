736a754fb2fcb592965c0ecceefee028af80d0c6
indre101 <zygaityte.indre@gmail.com>
2020-02-25 15:53:40 +0100

    added basic mark up for modal

--- script.js
+++ script.js
@@ -29,8 +29,6 @@ function selectHTMLelements() {
   return HTML;
 }
 
-
-
 function searchStudent(element, array) {
   element.addEventListener("input", (event) => {
     const searchResult = array.filter(element => (element.firstName + element.lastName).toLowerCase().includes(event.target.value))
@@ -133,13 +131,10 @@ function displayNewOrder(array) {
       allStudentsHTML[elementIndex].style.display = "none";
       difference--
     }
-
-
     for (let index = 0; index < array.length; index++) {
       addStudentProperties(allStudentsHTML[index], array[index]);
     }
   }
-
 }
 
 
@@ -213,118 +208,3 @@ function displayStudentListItems(student) {
   addStudentProperties(cln, student)
   selectHTMLelements().students.appendChild(cln);
 }
-
-
-// const studentTemplate = document.querySelector(".studentTemplate").content;
-// const students = document.querySelector(".students");
-
-
-// window.addEventListener("DOMContentLoaded", init);
-
-// const Student = {
-//   firstName: "",
-//   lastName: "",
-//   middleName: undefined,
-//   nickName: undefined,
-//   image: "",
-//   house: ""
-// }
-// const studentsArr = [];
-
-
-// function init() {
-//   fetch("https://petlatkea.dk/2020/hogwarts/students.json")
-//     .then(res => res.json())
-//     .then(data =>
-//       data.forEach(createStudentInfoCard)
-//     ).then(() => {
-//       studentsArr.forEach(student => {
-//         createStudentCards(student, studentsArr);
-//       });
-//     })
-//     .then(() => {
-//       const themeOptions = document.querySelectorAll(".theme");
-//       const themeOptionsArr = Array.from(themeOptions);
-//       themeOptions.forEach(option => {
-//         option.addEventListener("change", () => {
-//           studentsArr[themeOptionsArr.indexOf(option)].house = selectedHouse();
-//           const selectedStudentCard = document.querySelectorAll(".student")[
-//             themeOptionsArr.indexOf(option)
-//           ];
-//           const selectedStudentObject = studentsArr[themeOptionsArr.indexOf(option)];
-//           showStudentHouseAndModal(selectedStudentCard, selectedStudentObject);
-//         });
-//       });
-//     })
-
-// };
-
-
-
-// function createStudentInfoCard(student) {
-//   const fullNameWithoutWhitespaces = capitaliseAfterGapsHyphen(removeWhiteSpaces(student.fullname).toLowerCase())
-//   const studentCard = Object.create(Student);
-//   studentCard.firstName = findFirstName(fullNameWithoutWhitespaces);
-//   studentCard.lastName = lastName(fullNameWithoutWhitespaces);
-//   studentCard.middleName = getMiddleName(fullNameWithoutWhitespaces);
-//   studentCard.nickName = capitalise(getNickname(fullNameWithoutWhitespaces))
-//   studentCard.house = capitalise(removeWhiteSpaces(student.house).toLowerCase());
-//   studentsArr.push(studentCard)
-// }
-
-
-// function selectedHouse() {
-//   const selectedTheme = event.target;
-//   return selectedTheme.value;
-// }
-
-// function getNames(student) {
-//   let fullName;
-//   if (student.nickName && student.middleName) {
-//     fullName = `${student.firstName} Middle name ${student.middleName} Nick name ${student.nickName} ${student.lastName}`
-//   } else if (student.nickName) {
-//     fullName = `${student.firstName} Nick name ${student.nickName} ${student.lastName}`
-//   } else if (student.middleName) {
-//     fullName = `${student.firstName} Middle name ${student.middleName} ${student.lastName}`
-//   } else {
-//     fullName = `${student.firstName} ${student.lastName}`
-//   }
-//   return fullName;
-
-// }
-
-// function createStudentCards(student, data) {
-//   const clnStudent = studentTemplate.cloneNode(true);
-//   clnStudent.querySelector(".nameOftheStudent").textContent = `${student.firstName} ${student.lastName}`;
-//   clnStudent.querySelector(".textStudentName").textContent = getNames(student);
-//   clnStudent.querySelector(".number").textContent = data.indexOf(student) + 1;
-
-//   showStudentHouseAndModal(clnStudent, student);
-//   clnStudent.querySelectorAll(".theme option").forEach(option => {
-//     if (option.value == student.house.toLowerCase()) {
-//       option.selected = true;
-//     }
-//   });
-
-//   students.appendChild(clnStudent);
-// }
-
-// function showHideElement(element, classToAdd, classtoRemove) {
-//   element.classList.toggle(`${classToAdd}`);
-//   element.classList.toggle(`${classtoRemove}`);
-// }
-
-// function showStudentHouseAndModal(clnStudent, student) {
-//   clnStudent.querySelector(".house").textContent = student.house;
-//   const modal = clnStudent.querySelector(".modal");
-//   modal.dataset.crest = student.house.toLowerCase();
-//   clnStudent.querySelector(".textStudentHouse").textContent = student.house;
-
-//   clnStudent.querySelector(".mainStudentInfo").onclick = function () {
-//     showHideElement(modal, "d-flex", "d-none");
-//   };
-
-//   clnStudent.querySelector(".close").onclick = function () {
-//     showHideElement(modal, "d-flex", "d-none");
-//   };
-// }
